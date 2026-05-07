5bb65c255fecde2a4b198612505a5eab1e5583c6
Harshini <24b01a1223@svecw.edu.in>
2026-05-07 15:53:01 +0530

    Fixed RetrievalQA import

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 
 from langchain_groq import ChatGroq
 
